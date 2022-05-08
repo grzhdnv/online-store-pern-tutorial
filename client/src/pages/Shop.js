0ea80a032761f37e752f61180d58c5a8dded500d
mgrzhdnv <mgrazhdanov@gmail.com>
2022-05-08 18:18:34 +0300

    added TypeBar, BrandBar, DeviceList, DeviceItem

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import TypeBar from "../components/TypeBar";
+import BrandBar from "../components/BrandBar";
+import DeviceList from "../components/DeviceList";
 
 function Shop() {
-  return <div>Shop</div>;
+  return (
+    <Container>
+      <Row className="mt-2">
+        <Col md={3}>
+          <TypeBar></TypeBar>
+        </Col>
+        <Col md={9}>
+          <BrandBar />
+          <DeviceList />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Shop;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -5,17 +5,26 @@ export default class DeviceStore {
     this._types = [
       { id: 1, name: "Fridges" },
       { id: 2, name: "Smartphones" },
+      { id: 3, name: "Laptops" },
+      { id: 4, name: "TVs" },
     ];
     this._brands = [
       { id: 1, name: "Samsung" },
       { id: 2, name: "Apple" },
+      { id: 3, name: "Lenovo" },
+      { id: 4, name: "Asus" },
     ];
     this._devices = [
       { id: 1, name: "iPhone 12 pro", price: 25000, rating: 5, img: null },
       { id: 2, name: "iPhone 12 pro", price: 25000, rating: 5, img: null },
       { id: 3, name: "iPhone 12 pro", price: 25000, rating: 5, img: null },
       { id: 4, name: "iPhone 12 pro", price: 25000, rating: 5, img: null },
+      { id: 5, name: "iPhone 12 pro", price: 25000, rating: 5, img: null },
+      { id: 6, name: "iPhone 12 pro", price: 25000, rating: 5, img: null },
     ];
+
+    this._selectedType = {};
+    this._selectedBrand = {};
     makeAutoObservable(this);
   }
 
@@ -28,6 +37,14 @@ export default class DeviceStore {
   setDevices(devices) {
     this._devices = devices;
   }
+
+  setSelectedType(type) {
+    this._selectedType = type;
+  }
+  setSelectedBrand(brand) {
+    this._selectedBrand = brand;
+  }
+
   get types() {
     return this._types;
   }
@@ -36,5 +53,11 @@ export default class DeviceStore {
   }
   get devices() {
     return this._devices;
+  }
+  get selectedType() {
+    return this._selectedType;
+  }
+  get selectedBrand() {
+    return this._selectedBrand;
   }
 }
